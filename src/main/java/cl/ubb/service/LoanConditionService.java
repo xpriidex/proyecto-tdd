2134dc04c56ebf7d7b74f4c586283de35660532a
Felipe Cifuentes Espinosa <felcifue@alumnos.ubiobio.cl>
2017-07-10 14:08:51 -0400

    loanConditionService

--- src/main/java/cl/ubb/service/LoanConditionService.java
+++ src/main/java/cl/ubb/service/LoanConditionService.java
@@ -1,12 +1,42 @@
 package cl.ubb.service;
 
+import cl.ubb.dao.LoanConditionDao;
+import cl.ubb.dao.exceptions.CreateException;
+import cl.ubb.dao.exceptions.ReadErrorException;
+import cl.ubb.model.LoanCondition;
+import cl.ubb.service.exceptions.EmptyListException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by Felipe Cifuentes on 10-07-2017.
  */
 public class LoanConditionService {
+
+    @Autowired
+    private LoanConditionDao loanConditionDao;
+
+    public LinkedList<LoanCondition> getAll() throws EmptyListException {
+        List<LoanCondition> subjects = (List<LoanCondition>) loanConditionDao.getAll();
+        if(subjects.size()==0){
+            throw new EmptyListException();
+        }
+        return (LinkedList<LoanCondition>) loanConditionDao.getAll();
+    }
+
+    public LoanCondition get(String id) throws ReadErrorException {
+        if (!loanConditionDao.exist(id))
+            throw new ReadErrorException();
+
+        return loanConditionDao.get(id);
+    }
+
+    public void create(LoanCondition loanCondition) throws CreateException {
+        if (!loanConditionDao.exist(loanCondition.getIdentifier()))
+            throw new CreateException();
+        loanConditionDao.create(loanCondition);
+    }
 
 }
